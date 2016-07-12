e7d947a7b92f82b8b96195f46f9c6b7dcd32f555
chillinhen <c.hillen@gmx.net>
2016-07-12 09:34:05 +0200

    feinschliff

--- category.php
+++ category.php
@@ -1,34 +1,34 @@
 <?php get_header(); ?>
 <!-- section -->
-<section class="row">
-
-    <div class="main">
-
-        <h1><?php
-            _e('', 'heinzTheme');
-            single_cat_title();
-            ?></h1>
-        <p>
-            <em>
-            <?php echo category_description(); ?>
-            </em>
-        </p>
-        <div class="blogposts">
+<section class="container">
+    <?php if (have_posts()): ?>
+        
+            <h1>
+                <?php single_cat_title(); _e(' - Alle Beiträge aus dem Programm: ', 'heinzTheme');?>
+            </h1>
+            <p>
+                <em>
+                    <?php echo category_description(); ?>
+                </em>
+            </p>
+       <div class="blogposts">
             <?php
-            if (have_posts()): while (have_posts()) : the_post();
-                    get_template_part('partials/card');
-                endwhile;
-                get_template_part('pagination');
-            else:
-                get_template_part('partials/article', '404');
-            endif;
+            while (have_posts()) : the_post();
+                get_template_part('partials/article',  get_post_format());
+            endwhile;
             ?>
         </div>
+        <?php
+        get_template_part('pagination');
+    else:
+        ?>
+        <div class="main">
+        <?php get_template_part('partials/article', '404'); ?>
+        </div>
+<?php endif; ?>
+</section>
 
 
+<!-- /section -->
 
-
-    </div>
-    <!-- /section -->
-
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
--- sidebar-events.php
+++ sidebar-events.php
@@ -7,8 +7,8 @@
 			<div class="subcategories">
 			<h3><?php _e('Programmarchiv', 'heinzTheme'); ?></h3>
 			<?php foreach($categories as $category) :
-			    echo '<a class="catlink" href="' . get_category_link( $category->term_id ) . '" title="' . sprintf( __( "View all posts in %s" ), $category->name ) . '" ' . '>' . $category->name.'</a>';
-			    echo '<p>'. $category->description . '</p>';
+			    echo '<a class="catlink" href="' . get_category_link( $category->term_id ) . '#main-menu" title="' . sprintf( __( "View all posts in %s" ), $category->name ) . '" ' . '>' . $category->name.'</a>';
+			    echo '<p class="description">'. $category->description . '</p>';
 			endforeach;?>
 			</div>
 		<?php endif; ?>
